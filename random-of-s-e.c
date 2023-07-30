20a9b3febe04999d4558d0440c8a6dbbe8c63820
Beregszászi István <beistvan@gmail.com>
2023-07-30 14:12:39 +0200

    Random Number Of S And E

---
+++ random-of-s-e.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+int main(void) {
+    int s, e;
+    scanf("%d %d", &s, &e);
+    srand(time(NULL));
+    int rand_digit = rand();
+    printf("%d\n", s + rand_digit % (s - e + 1));
+    return 0;
+}
