cadc00f049bafff0e0e2d02e9b7a14cc06f691b5
Beregszászi István <beistvan@gmail.com>
2023-12-31 16:22:19 +0100

    Factorials From One Up To Ten

---
+++ factorials-from-one-up-to-ten.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+  printf("1! =       1\n");
+  printf("2! =       2\n");
+  printf("3! =       6\n");
+  printf("4! =      24\n");
+  printf("5! =     120\n");
+  printf("6! =     720\n");
+  printf("7! =    5040\n");
+  printf("8! =   40320\n");
+  printf("9! =  362880\n");
+  printf("10!= 3628800\n");
+  return 0;
+}
