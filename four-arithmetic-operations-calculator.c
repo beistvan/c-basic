201e15615d9c6d15e817a5298997bbc5dff168fe
Beregszászi István <beistvan@gmail.com>
2024-01-05 11:33:03 +0100

    Four Arithmetic Operations Calculator

---
+++ four-arithmetic-operations-calculator.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+  int op, a, b;
+  scanf("%c %d %d", &op, &a, &b);
+
+  switch (op) {
+    case '+': printf("%.2lf", (double)a + b); break;
+    case '-': printf("%.2lf", (double)a - b); break;
+    case '*': printf("%.2lf", (double)a * b); break;
+    case '/': printf("%.2lf", (double)a / b); break;
+    default: printf("ERROR!\n");
+  }
+
+  return 0;
+}
