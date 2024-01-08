baa412449345d8f1aa2ef6ae7228dc7771d78f41
Beregszászi István <beistvan@gmail.com>
2024-01-08 22:04:11 +0100

    Feet And Inches To Meters - Correct Space And Dot

--- feet-and-inches-to-meters.c
+++ feet-and-inches-to-meters.c
@@ -3,6 +3,6 @@
 int main() {
   int f, d;
   scanf("%d %d", &f, &d);
-  printf("%d'%d\" = %.2lfm.", f, d, (f * 12 + d) * 0.0254);
+  printf("%d'%d\" = %.2lf m", f, d, (f * 12 + d) * 0.0254);
   return 0;
 }
