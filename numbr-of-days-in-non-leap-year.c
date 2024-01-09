9055d757c6c29f79b1a8447d905143869d210b37
Beregszászi István <beistvan@gmail.com>
2024-01-09 01:35:47 +0100

    Numbr Of Days In Non Leap Year

---
+++ numbr-of-days-in-non-leap-year.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int month, days;
+    scanf("%d %d", &month, &days);
+    switch (--month) {
+        //case 12: days += 31;
+        case 11: days += 30;
+        case 10: days += 31;
+        case  9: days += 30;
+        case  8: days += 31;
+        case  7: days += 31;
+        case  6: days += 30;
+        case  5: days += 31;
+        case  4: days += 30;
+        case  3: days += 31;
+        case  2: days += 28;
+        case  1: days += 31;
+            printf("%d\n", days);
+            break;
+        default: printf("ERROR!\n");
+    }
+    return 0;
+}
