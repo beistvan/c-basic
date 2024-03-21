d99fb1f1bfede7f3188f317a9649915f18990e23
Beregszászi István <beistvan@gmail.com>
2024-03-21 08:17:17 +0100

    String Comparition: No Case Sensitive

--- string-comparition-no-case-sensitive.c
+++ string-comparition-no-case-sensitive.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-#define MAX_LEN 50
+#define STR_LEN 50
 
 int main() {
-  char str1[MAX_LEN], str2[MAX_LEN];
-  scanf("%s %s", str1, str2);
-  if (strlen(str1) != strlen(str2) || strcmp(str1, str2) != 0)
-    printf("no");
-  else
-    printf("yes");
-  return 0;
+    char str1[STR_LEN], str2[STR_LEN];
+    gets(str1); gets(str2);
+    if (strcasecmp(str1, str2))
+        printf("no");
+    else
+        printf("yes");
+    return 0;
 }
