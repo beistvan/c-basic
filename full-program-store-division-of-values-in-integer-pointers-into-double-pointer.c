af60082d9f5c1b8d770f6ab318b30da97a9d19c2
Beregszászi István <beistvan@gmail.com>
2024-03-21 08:56:02 +0100

    Full Program: Store Division Of Values In Integer Pointers Into Double Pointer

---
+++ full-program-store-division-of-values-in-integer-pointers-into-double-pointer.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+  int a=0, b=0;
+  double f = 0; 
+  scanf("%d %d", &a, &b);
+  double * p_dbl = &f;
+  int * p_1 = &a;
+  int * p_2 = &b;
+
+  *p_dbl = (double)(*p_1)/(*p_2);    
+
+  printf("%.3f",f);
+  return 0;
+}
