69f4bde95b504c04d0b951954d3ba1d01639f24d
Beregszászi István <beistvan@gmail.com>
2024-03-21 08:02:50 +0100

    Greeting Program, That Concatenates The Strings

---
+++ greeting-program-that-concatenates-the-strings.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+    char str1[40] = "Hello, "; 
+    char str2[20];    
+    scanf("%s", str2);
+    strcat(str1, str2);
+    printf("%s", str1);
+    return 0;
+}
